3b7ffeee186eb9cabd79369bd51b6f8b671d842c
Stephen <stevriss22@gmail.com>
2021-11-04 12:47:45 -0400

    commit tree 34e65a458833593f1ee97136520646c3411b62e3

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,48 @@
 # your code goes here
+class Person 
+    @@people = []
+
+    attr_accessor :bank_account, :hygiene
+    
+    def happiness(happiness)
+        @happiness = happiness
+    end
+
+    def initialize(name)
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+        @name = name 
+
+        @@people << name
+
+        
+    end
+
+    # def happiness
+    #     if @happiness > 10
+    #         puts"error"
+    #     elsif @happiness < 0
+    #         puts "error"
+    #     else
+    #     end
+
+    # end
+
+    
+    def happiness
+        if @happiness > 10
+            puts"error"
+        elsif @happiness < 0
+            puts"error"
+        else
+            @happiness
+        end
+    end
+
+
+
+    def name
+        @name
+    end
+end
